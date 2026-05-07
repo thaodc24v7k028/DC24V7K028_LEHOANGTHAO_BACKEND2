f0285e07fec3f848101d9bc8a628a05b56843ab4
thaodc24v7k028 <thaodc24v7k028@vlvh.ctu.edu.vn>
2026-05-07 16:11:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const passport = require("./app/config/passport");
 
 app.use(
   session({
-    secret: "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",
+    secret: "GOOGLE_CLIENT_SECRET",
     resave: false,
     saveUninitialized: true,
   })
